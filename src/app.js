cf8c6d756d378b5b96b8ce7acbd8b57f76b2acfb
Christeen <chrispaul1311@gmail.com>
2025-07-04 21:32:22 +0530

    .

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@
 const express = require('express');
 const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/user/:id",(req,res)=>{
+    console.log(req.params)
     res.send("hello world")
 })
+
+app.get("/abc" , (req,res)=>{
+    res.send({name:"Christeen Paul"});
+})
+
+app.use("/hel",(req,res)=>{
+  res.send("world hello world")
+})
+
 
 app.listen(3000,()=>{
     console.log("App listing to port 3000")
