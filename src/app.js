487b34628290f5881ea08610611eba225e642630
Christeen <chrispaul1311@gmail.com>
2025-07-06 15:31:37 +0530

    User.find

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const connectDB= require("./config/database.js")
-const User=require("./models/user.js")
+const User=require("./models/user.js");
+
 
 app.use(express.json());
 
@@ -16,6 +17,34 @@ app.post('/signup',async(req,res)=>{
      }
 
 
+})
+
+app.post('/user', async(req,res)=>{
+   const user= req.body.emailId;
+   try{
+    const getuser=await User.find({emailId:user})
+     if(getuser.length==0){
+        res.status(404).send("User not found")
+     }else{
+        res.send(getuser)
+     }
+   }catch(err){
+    res.status(404).send("Someting went wrong")
+   }
+})
+
+app.post('/feed', async(req,res)=>{
+
+   try{
+     const getuser=await User.find({})
+     if(getuser.length==0){
+        res.status(404).send("User not found")
+     }else{
+        res.send(getuser)
+     }
+   }catch(err){
+    res.status(404).send("Someting went wrong")
+   }
 })
 
 connectDB()
